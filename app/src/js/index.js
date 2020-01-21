54ff3d77f6aec6c9f5f1cf0d49adf7cfaf9298a7
Marc Juul <marc@juul.io>
2020-01-21 02:08:54 -0800

    added OPF parsing of ISBN, other Onix CodeList 5 values, and refined meta values

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,4 +1,5 @@
 import {h, render, Component} from 'preact';
+import {hyphenate as formatISBN} from 'beautify-isbn';
 import parseLanguage from './parse_language.js';
 
 import Root from './components/Root.js';
@@ -8,6 +9,32 @@ var app = {
 };
 
 window.app = app;
+
+// TODO actually make use of this
+// Used to parse OPF identifiers
+// https://www.stison.com/onix/codelists/onix-codelist-5.htm
+const onixCodeList5 = {
+  1: "Proprietary",
+  2: "ISBN-10",
+  3: "GTIN-13",
+  4: "UPC",
+  5: "ISMN-10",
+  6: "DOI",
+  13: "LCCN",
+  14: "GTIN-14",
+  15: "ISBN-13",
+  17: "Legal deposit number",
+  22: "URN",
+  23: "OCLC number",
+  25: "ISMN-13",
+  26: "ISBN-A",
+  27: "JP e-code",
+  28: "OLCC number",
+  29: "JP Magazine ID",
+  30: "UPC12+5",
+  31: "BNF Control number",
+  35: "ARK"
+};
 
 // supported cover image mimetypes
 const supportedCoverMediaTypes = [
@@ -71,21 +98,64 @@ class OPF {
     return els[0];
   }
 
-  // TODO also figure out how to parse this:
-  // (from the EPUB 3.0.1 standard)
-  /*
-        <dc:identifier 
-              id="isbn13">urn:isbn:9780741014559</dc:identifier>
-        <meta refines="#isbn13" 
-              property="identifier-type" 
-              scheme="onix:codelist5">15</meta>
-        
-        <dc:identifier id="isbn10">0-7410-1455-6</dc:identifier>
-        <meta refines="#isbn10" 
-              property="identifier-type" 
-              scheme="onix:codelist5">2</meta>
-  */
-  
+  // Tags can refine previous tags, e.g:
+  // <dc:identifier id="foo" some-prop="florp">txt</dc:identifier>
+  // <meta refines="#foo" property="my-prop" scheme="my:scheme">some-val</meta>
+  // <meta refines="#foo" property="my-prop2" scheme="my:scheme">some-val2</meta>
+  // <meta refines="#foo" property="my-prop3">some-val3</meta>
+  // This finds any tags that refine a previous tag
+  // and spits out the integrated data as an object, e.g. for the above:
+  // {
+  //   "noScheme": {
+  //     id: "foo",
+  //     some-prop: "florp",
+  //     my-prop3: "some-val3"
+  //   },
+  //   "my:scheme": {
+  //     my-prop": "some-val",
+  //     my-prop2": "some-val2"
+  //   }
+  // }
+  refineMeta(element) {
+    const attrs = element.getAttributeNames();
+    const o = {
+      noScheme: {}
+    };
+    var i;
+    var attr;
+    var scheme = o.noScheme;
+    for(i=0; i < attrs.length; i++) {
+      attr = attrs[i]
+      scheme[attr] = element.getAttribute(attr);
+    }
+    if(!scheme.id) return o;
+
+
+    var els = this.doc.querySelectorAll("package > metadata meta[refines=\\#"+scheme.id+"]");
+    var el;
+    for(i=0; i < els.length; i++) {
+      el = els[i];
+      scheme = el.getAttribute('scheme') || 'noScheme';
+      attr = el.getAttribute('property');
+      if(!attr) continue;
+      
+      if(!o[scheme]) o[scheme] = {};
+      scheme = o[scheme];
+      scheme[attr] = el.textContent;
+    }
+    return o;
+  }
+
+  // Get the EPUB's UUID
+  // and properly formatted ISBN-10 or ISBN-13 (if present)
+  // and any other Onix Code List 5 identifiers.
+  // 
+  // Returns e.g:
+  // {
+  //   'UUID': '124214214',
+  //   'ISBN-10': '0-330-32002-5',
+  //   'DOI': '10.1038/nature04586'
+  // }
   parseIdentifiers() {
     var el = this.doc.querySelector('package');
     if(!el) return {};
@@ -97,21 +167,75 @@ class OPF {
     const els = this.getMetas('dc:identifier');
     if(!els.length) return {};
     const o = {};
+    var attrs;
+    var attr;
+    var val;
     var i;
     for(i=0; i < els.length; i++) {
       el = els[i];
-      if(el.getAttribute('opf:scheme').toUpperCase() === 'ISBN') {
-        const isbn = el.textContent.replace(/[^\d]+/, '');
-        if(isbn.length === 10) {
-          o.isbn10 = isbn;
-        } else if(isbn.length === 13) {
-          o.isbn13 = isbn;
+
+      // Get UUID
+      //
+      // <dc:identifier id="uuid_id" opf:scheme="uuid">8ad7cb26-947b-4e7f-89e9-40fd8a4e530a</dc:identifier>
+      //
+      if(epubID && el.getAttribute('id') === epubID) {
+        o['UUID'] = el.textContent;
+        continue;
+      }
+
+      // Parse older form of ISBN
+      //
+      // <dc:identifier opf:scheme="ISBN">0-330-32002-5</dc:identifier>
+      //
+      val = el.getAttribute('opf:scheme');
+      if(val && val.toUpperCase() === 'ISBN') {
+        val = el.textContent.replace(/[^\d]+/g, '');
+        if(val.length === 10) {
+          o['ISBN-10'] = formatISBN(val);
+        } else if(val.length === 13) {
+          o['ISBN-13'] = formatISBN(val);
         }
         continue;
       }
-      if(epubID && el.getAttribute('id') === epubID) {
-        o.uuid = epubID;
+
+      // Parse any Onix Code List 5 identifiers
+      // including ISBN-10 and ISBN-13
+      //
+      // <dc:identifier id="isbn13">urn:isbn:9780741014559</dc:identifier>
+      // <meta refines="#isbn13" 
+      //   property="identifier-type" 
+      //   scheme="onix:codelist5">15</meta>
+      //
+      attrs = this.refineMeta(el);
+      if(!attrs) continue;
+      if(attrs['onix:codelist5']) {
+        attr = attrs['onix:codelist5']['identifier-type'];
+        if(!attr) continue;
+        // look up what type of identifier this is
+        attr = onixCodeList5[attr];
+        if(!attr) continue;
+        o[attr] = el.textContent;
+        // Try to format ISBNs nicely
+        if(attr === 'ISBN-10' || attr === 'ISBN-13') {
+          o[attr] = o[attr].replace(/[^\d]+/g, '');
+          o[attr] = formatISBN(o[attr]);
+        }
+        continue;
       }
+
+      // Non-standard ISBN format seen in some epubs
+      //
+      // <dc:identifier id="isbn9781509830718">9781509830718</dc:identifier>
+      //
+      if(attrs['noScheme']['id'].match(/^isbn/i)) {
+        val = el.textContent.replace(/[^\d]+/g, '');
+        if(val.length === 10) {
+          o['ISBN-10'] = formatISBN(val);
+        } else if(val.length === 13) {
+          o['ISBN-13'] = formatISBN(val);
+        }
+      }
+      
     }
     return o;
   }
@@ -126,7 +250,7 @@ class OPF {
       const href = el.getAttribute('href');
       if(!href) return false;
       if(href.indexOf('.') < 0) return false;
-      const ext = href.replace(/.*\./, '').toLowerCase()
+      const ext = href.replace(/.*\./g, '').toLowerCase()
       if(supportedCoverFileTypes.indexOf(ext) >= 0) {
         return supportedCoverFileTypeConv[ext];
       }
@@ -201,6 +325,7 @@ class OPF {
     
     this.title = this.getMeta('dc:title', true)
     this.identifiers = this.parseIdentifiers();
+    console.log("IDs:", this.identifiers);
     
     const lang = this.getMeta('dc:language', true)
     if(lang) {
